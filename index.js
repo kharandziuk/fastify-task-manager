92e2a98aad9458d47bfda0aec88ea3da64ac956e
Max Kharandziuk <max.kharandziuk@gmail.com>
2019-07-14 13:06:51 +0200

    add a prototype of jwt-token login

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const Fastify = require('fastify')
 function buildFastify () {
   const fastify = Fastify()
 
+  fastify.register(require('fastify-jwt'), {
+      secret: 'supersecret'
+  })
+
+  fastify.post('/login', async (req, reply) => {
+    const { username, password }  = req.body
+    if(username === 'username' && password === 'password') {
+      const token = fastify.jwt.sign({ id: 'ID' })
+      return { token }
+    } else {
+      reply.code(401)
+      return {error: 'invalid username or password'}
+    }
+  })
+
+
   fastify.get('/', function (request, reply) {
     reply.send({ hello: 'world' })
   })
--- test/integration.spec.js
+++ test/integration.spec.js
@@ -1,6 +1,7 @@
 const supertest = require('supertest')
 const { expect } = require('chai')
-const buildFastify = require('./index')
+const buildFastify = require('../index')
+
 
 describe('server', () => {
   let fastify
@@ -17,5 +18,27 @@ describe('server', () => {
       .get('/')
     expect(response.statusCode).eql(200)
     expect(response.body).eql({ hello: 'world' })
+  })
+
+  it('can login w valid credentials', async () => {
+    const response = await supertest(fastify.server)
+      .post('/login')
+      .send({
+        username: 'username',
+        password: 'password'
+      })
+    expect(response.statusCode).eql(200)
+    expect(response.body).to.have.key('token')
+  })
+
+  it('cant login wo valid credentials', async () => {
+    const response = await supertest(fastify.server)
+      .post('/login')
+      .send({
+        username: 'invalid',
+        password: 'invalid'
+      })
+    expect(response.statusCode).eql(401)
+    expect(response.error).eql
   })
 })
